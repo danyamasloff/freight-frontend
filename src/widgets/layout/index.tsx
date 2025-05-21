07678f445fdaa0985a5cbda1e3020b5069c721a1
danyamasloff <danyamasloff@yandex.ru>
2025-05-21 20:40:52 +0300

    feat: обновлен основной макет приложения с компонентами Aceternity UI

--- src/widgets/layout/index.tsx
+++ src/widgets/layout/index.tsx
@@ -1,20 +1,28 @@
 import { Outlet } from 'react-router-dom'
-import { AppSidebar } from '@/components/app-sidebar'
-import { SidebarProvider, SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { AceternitySidebar } from '@/components/aceternity-sidebar'
 import { Header } from '@/widgets/header'
+import { GridBackground } from '@/components/grid-background'
 
 export function Layout() {
     return (
         <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <Header />
-                <main className="flex-1 overflow-hidden">
-                    <div className="p-6 h-full">
-                        <Outlet />
-                    </div>
-                </main>
-            </SidebarInset>
+            <div className="flex h-screen w-full overflow-hidden bg-background">
+                <AceternitySidebar />
+                <div className="flex flex-1 flex-col overflow-hidden w-full">
+                    <Header />
+                    <main className="flex-1 w-full overflow-auto relative">
+                        <GridBackground
+                            containerClassName="absolute inset-0"
+                            className="fixed inset-0 z-0"
+                        >
+                            <div className="p-6 w-full h-full max-w-full relative z-10">
+                                <Outlet />
+                            </div>
+                        </GridBackground>
+                    </main>
+                </div>
+            </div>
         </SidebarProvider>
     )
 }
